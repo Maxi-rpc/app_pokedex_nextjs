029d046a4dd5af525299a68bb78e3f2df588bc9b
Maximiliano <maxirpc2607@gmail.com>
2023-11-23 09:09:20 -0300

    parseo de datos

--- src/services/api_services.js
+++ src/services/api_services.js
@@ -11,13 +11,21 @@ export const get_pokemons = (offset = 0, limit = 10) => {
 	return axios.get(API_POKEMONS);
 };
 
-export const get_pokemon_by_name = async (name = "lucario") => {
-	const res = await axios.get(API_POKEMON + name);
+export const get_pokemon_by_name = async (pokename = "lucario") => {
+	const res = await axios.get(API_POKEMON + pokename);
 	const { status, data } = res;
-	console.log(data);
-};
 
-const parserData = (status, data) => {
-	const datos = {};
-	return { status, datos };
+	const { id, name, height, weight, abilities, sprites, stats, types } = data;
+
+	const datos = {
+		id: id,
+		name: name,
+		height: height,
+		weight: weight,
+		abilities: abilities,
+		sprites: sprites,
+		stats: stats,
+		types: types,
+	};
+	return { stats, datos };
 };
